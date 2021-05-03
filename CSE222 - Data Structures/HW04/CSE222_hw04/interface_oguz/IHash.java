05c85f90d44c1a8355d9a602ad4b2690b63a733f
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-05-03 23:28:17 +0300

    Create HW04 files.

---
+++ CSE222 - Data Structures/HW04/CSE222_hw04/interface_oguz/IHash.java
@@ -0,0 +1,10 @@
+
+package CSE222_hw04.interface_oguz;
+
+/**
+ * IHash
+ */
+public interface IHash<E> {
+
+    
+}
---
+++ CSE222 - Data Structures/HW04/CSE222_hw04/src_oguz/Hash.java
@@ -0,0 +1,7 @@
+package CSE222_hw04.src_oguz;
+
+import CSE222_hw04.interface_oguz.IHash;
+
+public class Hash<E> implements IHash<E> {
+    
+}
