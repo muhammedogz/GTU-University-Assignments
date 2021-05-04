636bd104653bade69f007a900e6a320c95845e98
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-05-04 22:41:53 +0300

    Test all possiblites

--- CSE222 - Data Structures/HW04/Test.java
+++ CSE222 - Data Structures/HW04/Test.java
@@ -170,16 +170,12 @@ public class Test {
         Heap<Integer> heap2 = new Heap<Integer>();
         
 
-        heap.add(15);
-        heap.add(2);
-        heap.add(8);
-        heap.add(5);
+        heap.add(15); heap.add(2);
+        heap.add(8); heap.add(5);
         heap.add(4);
         
-        heap2.add(12);
-        heap2.add(1);
-        heap2.add(7);
-        heap2.add(8);
+        heap2.add(12); heap2.add(1);
+        heap2.add(7); heap2.add(8);
         
         heap.removeIthBiggestElement(2);
         System.out.println("Heap1 = " + heap.toString());
