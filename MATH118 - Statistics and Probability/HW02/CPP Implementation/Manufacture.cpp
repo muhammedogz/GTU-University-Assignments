b2a9bb740f55575867cb1055efdbfb0bddce37a2
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-06-01 02:49:28 +0300

    Implement readFile function

--- MATH118 - Statistics and Probability/HW02/CPP Implementation/Manufacture.cpp
+++ MATH118 - Statistics and Probability/HW02/CPP Implementation/Manufacture.cpp
@@ -17,11 +17,63 @@ void Manufacture::loadFile(string loadFile)
         return;
     }
 
+    // use this for keeping lines
     string line;
-    int i = 0;
-    while (getline(fp, line, '\t'))
+    // use this if data is not initialized yet
+    bool initialize = true;
+
+    // iterate over file by reading line by line
+    while (getline(fp, line))
     {
-        if (i++ == 10) break;
-        cout << line << endl;
+        // if line length equal to zero, continue
+        if (line.length() == 0) 
+            continue;
+
+        // create a row vector
+        vector<int> row;
+
+        // use stringstream to parse string
+        stringstream temp(line);
+        
+        // use this for each parsed string 
+        string str;
+
+        // skip first line
+        bool firstTime = false;
+
+        // read line by delimeter tab
+        while(getline(temp, str, '\t'))
+        {
+            if (!firstTime)
+            {
+                firstTime = true;
+                continue;
+            }
+
+            row.push_back(atoi(str.c_str()));
+        }    
+
+        // initialize data value if not initialized yet
+        if (initialize)
+        {
+            for (int i = 0; i < row.size(); i++)
+            {
+                data.push_back(map<int,int>());
+            }
+            initialize = false;
+        } 
+
+        // keep year value
+        int year = row[0];
+
+        // use a loop to read cases and assign with companies
+        for (int i = 0; i < row.size() - 1; i++)
+        {
+            data[i][year] = row[i+1];
+        }
     }
+
+    // void function
+    // end of the function, data value will fill with companies and their cases
+
 }
