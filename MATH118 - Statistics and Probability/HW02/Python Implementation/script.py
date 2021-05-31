6171fca85b5d3988623f037379c332eefb5bb653
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-05-31 14:12:45 +0300

    Implement part-a and part-b

--- MATH118 - Statistics and Probability/HW02/Python Implementation/script.py
+++ MATH118 - Statistics and Probability/HW02/Python Implementation/script.py
@@ -1,16 +1,25 @@
 #! /usr/bin/env python3 
 
-
+from asyncio import events
+from re import I, T
+from time import time
 from typing import Dict, List
 
 
-def load_data(f : str) -> List[Dict[str,str]]:
-    data : List[Dict[str,str]] = []
+def load_data(f : str) -> List[Dict[int,int]]:
+    """Load all data from file
 
-    for i in range(20):
-        temp : Dict[str,str] = {}
-        data.append(temp)
+    Args:
+        f (str): Filename path/name to find file
 
+    Returns:
+        List[Dict[int,int]]: Return list od dict for keeping data
+    """
+
+    data : List[Dict[int,int]] = []
+
+    # keep for initializing
+    flag : bool = True
 
     with open(f, mode="r") as file:
         text = file.readlines()
@@ -26,18 +35,97 @@ def load_data(f : str) -> List[Dict[str,str]]:
             l = l[1:len(l)]
 
             # determine year
-            year = l[0]
+            year = int(l[0])
+
+            # if data is not initalized, (flag == True)
+            # Initialize data with length
+            if flag:
+                for i in range(len(l) - 1):
+                    temp : Dict[int,int] = {}
+                    data.append(temp)
+                    flag = False
 
             # go all info and assign year values to companies
             for i in range(len(l) - 1):
-                data[i][year] = l[i+1]
-  
-
+                data[i][year] = int(l[i+1])
 
     return data
 
-data = load_data("manufacturing_defects.txt")
 
-for company in data:
-    print(company)
+def case_table(data : List[Dict[int,int]]) -> List[int]:
+    """Get case infos from data
+
+    Args:
+        data (List[Dict[int,int]]): Data to handled
+
+    Returns:
+        List[int]: Return total cases for all companies in a List
+    """
+    l : List[int] = [x * 0 for x in range(5)]
+
+    # for each company in data
+    for company in data:
+        # for each case values
+        for val in company.values():
+            l[val] += 1
+    
+    return l
+
+def part_a(l : List[int]) -> None:
+    """Print answer of part a
+
+    Args:
+        l (List[int]): Used list to print
+    """
+    i : int = 0
+    print("Part-a")
+    print("Num Of Def | Total")
+    for val in l:
+        print("{} \t| {}".format(i,val))
+        i+=1
+        
+        
+def part_b(l : List[int]) -> None:
+    """Print answer of b
+
+    Args:
+        data (List[Dict[int,int]]): Use data to find how many time
+        l (List[int,int]): Use list to find total cases
+    """
+    # keep all events
+    all_times : int = 0
+    # keep total
+    total : int = 0
+
+    power : int = 0
+    for num in l:
+        total += num
+        all_times += num * power
+        power += 1
+
+    print("Part-b")
+    print("(Events = {})/(Times = {}) = {}".format(all_times, total, float(all_times/total)))
+
+
+    
+
+
+
+
+
+
+
+
+# read file and load data
+data = load_data("manufacturing_defects.txt")
+# load all cases
+l = case_table(data)
+
+part_a(l)
+part_b(l)
+
+
+
+
+
 
