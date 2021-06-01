904af60e1d27202b815041ed32d25fc20548019f
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-06-01 03:22:30 +0300

    Finish Manufactura class

--- MATH118 - Statistics and Probability/HW02/CPP Implementation/main.cpp
+++ MATH118 - Statistics and Probability/HW02/CPP Implementation/main.cpp
@@ -3,10 +3,27 @@
 
 using namespace std;
 
+// results for part-a
+void part_a();
+
 int main()
 {
     Manufacture manufacture;
     manufacture.loadFile("../manufacturing_defects.txt");
+    part_a();
     
 }
 
+void part_a(Manufacture manufacture)
+{
+    // load all value with counting
+    manufacture.countCases();
+
+    vector<int> list = manufacture.getList();
+
+    cout << "Case \t - Count" << endl;
+    for (int i = 0; i < list.size(); i++)
+    {
+        cout << i << "\t - " << list[i] << endl;
+    }
+}
