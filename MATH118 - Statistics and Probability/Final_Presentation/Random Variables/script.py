285899fcdcb1971adf8b039a01f5d2bf9b7721c2
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-06-12 18:01:30 +0300

    Add gitignore and script

---
+++ MATH118 - Statistics and Probability/Final_Presentation/Random Variables/script.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3 
+
+from __future__ import division
+# import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# import scipy
+
+
+class DiscreteRandomVariable:
+    def __init__(self, a: int = 0, b : int = 1):
+        self.variableType = ""
+        self.low = a
+        self.high = b
+        return
+    def draw(self, numberOfSamples : int):
+        samples = np.random.randint(self.low, self.high, numberOfSamples)
+        return samples
+
+DieRolls = DiscreteRandomVariable(1, 6)
+plt.hist(DieRolls.draw(10), bins = [1,2,3,4,5,6,7], align = 'mid')
+plt.xlabel('Value')
+plt.ylabel('Occurrences')
+plt.legend(['Die Rolls'])
+plt.savefig("dice.png", dpi=200)
