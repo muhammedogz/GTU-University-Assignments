9c553a108b36c194be3762dc447741b79810277c
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2021-11-13 19:13:04 +0300

    Refactor read int array

--- CSE331 - Computer Organization/HW02/read_file_to_array.asm
+++ CSE331 - Computer Organization/HW02/read_file_to_array.asm
@@ -12,7 +12,7 @@ str_space: .asciiz "space\n"
 str_zero: .asciiz "zero\n"
 
 new_line: .asciiz "\n"
-comma: .asciiz ","
+comma: .asciiz " "
 
 .text
 
@@ -27,57 +27,36 @@ main:
     while:
         lb $t2, 0($s0) # load char
 
-        beq $t2, $zero, end_arr
-        beq $t2, 13, end_arr # carriage return / new line
-        beq $t2, '\n', end_arr
-        beq $t2, ' ', new_int
+        beq $t2, $zero, end_arr # end of file
+        beq $t2, 13, end_arr # end of line carriage return / new line
+        beq $t2, ' ', new_int # end of word
 
 
         jal atoi
+
         j while
 
         end_arr:
-            li $v0, 4
-            la $a0, str_carriage
-            syscall
-
             sw $t1, arr($t4) # store in array
             addi $t3, $t3, 1 # increment array size
             addi $t4, $t4, 4 # increment array address location
 
-            addi $t5, $zero, 0
-            
-            whilePrint:
-                beq $t5, $t4, endPrint
+            # jal printArr  
 
-                lb $t9, arr($t5)
+            # call calculation formula here
 
-                li $v0, 1
-                move $a0, $t9
-                syscall
-
-                li $v0, 4
-                la $a0, comma
-                syscall
-
-                addi $t5, $t5, 4
-
-                j whilePrint
-
-            endPrint:
-
-            li $v0, 4
-            la $a0, new_line
-            syscall
-
+            lb t2, 0($s0) # load char
             addi $t3, $zero, 0 # t3 = 0, keeps array size
             addi $t4, $zero, 0 # t4 = 0, keeps array address location
 
-            beq $t2, 13, increment_one 
-            beq $t2, '\n', increment_one
+            beq $t2, 13, increment_two # it should pass two space
 
             j end
 
+        increment_two:
+            addi $s0, $s0, 2
+
+            j while
 
         new_int:
             sw $t1, arr($t4) # store in array
@@ -85,40 +64,16 @@ main:
             addi $t3, $t3, 1 # increment array size
             addi $t4, $t4, 4 # increment array address location
 
-            li $v0, 4
-            la $a0, str_newline
-            syscall
-
-            li $v0, 1
-            move $a0, $t4
-            syscall
-
-            j increment_one
-
-        increment_one:
             addi $s0, $s0, 1
 
             j while
-
-        # _space:
-        #     la $a0, str_space
-        #     j end
-        # _carriage:
-        #     la $a0, str_carriage
-        #     j end
-
         
     end:
-
-    li $v0, 4
-    syscall
 
 
     # end of main
     li $v0, 10
     syscall
-
-
 
 
 
@@ -144,44 +99,56 @@ openFileAndRead:
     la $s0, buffer   # keep address at $s0
 
     jr $ra  
-    
+        
 
 atoi:
     # la $t0, str # load string address
     addi $t1, $zero, 0 # t1 = res = 0
 
-    whileAtoi:
+        whileAtoi:
 
-        lb $t2, 0($s0) # load char
+            lb $t2, 0($s0) # load char
 
-        beq $t2, $zero, endAtoi
-        beq $t2, '\n', endAtoi
-        beq $t2, 13, endAtoi # carriage return / new line
-        beq $t2, ' ', endAtoi
+            beq $t2, $zero, endAtoi # end of file
+            beq $t2, 13, endAtoi # end of line
+            beq $t2, ' ', endAtoi # end of word
 
-        addi $t2, $t2, -48 # convert to char to int
+            addi $t2, $t2, -48 # convert to char to int
 
-        li $v0, 1
-        move $a0, $t2
-        syscall
+            mul $t1, $t1, 10 # res *= 10
+            add $t1, $t1, $t2 # res += temp
 
-        li $v0, 4
-        la $a0, val
-        syscall
+            addi $s0, $s0, 1 # go to next char
+        
+            j whileAtoi
 
-        mul $t1, $t1, 10 # res *= 10
-        add $t1, $t1, $t2 # res += temp
-
-        addi $s0, $s0, 1 # go to next char
-    
-        j whileAtoi
-
-    endAtoi:
-
-    li $v0, 1
-    move $a0, $t1
-    syscall
+        endAtoi:
 
     jr $ra
 
 
+
+printArr:
+    addi $t5, $zero, 0  
+    addi $t6, $zero, 0
+        whilePrint:
+            beq $t5, $t3, endPrint
+
+            lb $t9, arr($t6)
+
+            li $v0, 1
+            move $a0, $t9
+            syscall
+
+            li $v0, 4
+            la $a0, comma
+            syscall
+
+            addi $t5, $t5, 1
+            addi $t6, $t6, 4
+
+            j whilePrint
+
+        endPrint:
+
+    jr $ra
