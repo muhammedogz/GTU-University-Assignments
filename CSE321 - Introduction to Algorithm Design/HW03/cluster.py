353103b98f7e0814a7f720ae422a82d0e3bdbead
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2021-12-19 18:24:24 +0300

    Add second question

--- CSE321 - Introduction to Algorithm Design/HW03/cluster.py
+++ CSE321 - Introduction to Algorithm Design/HW03/cluster.py
@@ -16,7 +16,18 @@ def mostProfitableCluster(array: List[int]) -> List[int]:
     # return max profit cluster
     return maxProfitCluster
 
-print(mostProfitableCluster([3,-5,2,11,-8,9,-5]))
-print(mostProfitableCluster([3,-5,2,11,-8,9,-5,3]))
+cluster = mostProfitableCluster([3,-5,2,11,-8,9,-5])
+print(cluster)
     
 
+# find most valuable item in given array with divide and conquer
+def mostProfitableItem(array: List[int]) -> int:
+    if len(array) == 1:
+        return array[0]
+    else:
+        mid = len(array) // 2
+        left = mostProfitableItem(array[:mid])
+        right = mostProfitableItem(array[mid:])
+        return max(left, right)
+
+print(mostProfitableItem(cluster))
