d3e1b751fdd6e9fd2ea460e7463908085555a5d5
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2022-01-07 15:30:39 +0300

    Add 4th question answer

--- CSE321 - Introduction to Algorithm Design/HW04/find_rop.py
+++ CSE321 - Introduction to Algorithm Design/HW04/find_rop.py
@@ -1,45 +1,57 @@
 from typing import List
 
 
-def reversePairs(nums: List[int]) -> int:
-    """
-    :type nums: List[int]
-    :rtype: int
-    """
-    def msort(A : List[int]) -> List[int]:
-        # merge sort body
-        L = len(A)
-        if L <= 1:                          # base case
-            return A
-        else:                               # recursive case
-            return merger(msort(A[:int(L/2)]), msort(A[int(L/2):]))
-    def merger(left: List[int], right: List[int]) -> List[int]:
-        l, r = 0, 0                         # increase l and r iteratively
-        while l < len(left) and r < len(right):
-            if left[l] <= 2*right[r]:
-                l += 1
-            else:
-                self.cnt += len(left)-l     # COUNT here
-                r += 1
-        
-        res = []                            # merger
-        i, j = 0, 0
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                res += left[i],
-                i += 1
-            else:
-                res += right[j],
-                j += 1
-        
-        while i != len(left):
-            res += left[i],
-            i += 1
-        while j != len(right):
-            res += right[j],
-            j += 1
-        
-        return res
+# a divide function
+def divide(lst : List[int], low : int, high : int) -> int:
+    if (low >= high):
+        return 0
 
-    msort(nums)
-    return 5
+    mid = low + (high - low) // 2
+
+    left = divide(lst, low, mid)
+    right = divide(lst, mid + 1, high)
+
+    mergeResult = merge_like(lst, low, mid, high)
+
+    return left + right + mergeResult
+
+def merge_like(lst : List[int], leftInd : int, midInd : int, rightInd : int) -> int:
+    leftLst = lst[leftInd:midInd + 1]
+    rightLst = lst[midInd + 1: rightInd + 1]
+
+    i = 0
+    j = 0
+    k = leftInd
+    count = 0
+
+    while (i < len(leftLst) and j < len(rightLst)):
+        if (leftLst[i] <= rightLst[j]):
+            lst[k] = leftLst[i]
+            i += 1
+        else:
+            lst[k] = rightLst[j]
+            j += 1
+            count += midInd + 1 - leftInd - i
+        k += 1
+        
+    while (j < len(rightLst)):
+        lst[k] = rightLst[j]
+        j += 1
+        k += 1
+
+    while (i < len(leftLst)):
+        lst[k] = leftLst[i]
+        i += 1
+        k += 1
+    
+    return count
+
+def reversePairCount(lst : List[int]) -> int:
+    return divide(lst, 0, len(lst) - 1)
+
+
+# test
+print(reversePairCount([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+print(reversePairCount([5,1,2,3,4]))
+print(reversePairCount([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]))
+print(reversePairCount([15,6,4,33,4,5,12,5,6,7,8,3,5,2]))
