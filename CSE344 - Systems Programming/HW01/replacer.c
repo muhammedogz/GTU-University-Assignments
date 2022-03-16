53fe729ce28967e26df182ec4a4074de6c0cae1d
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2022-03-16 04:06:32 +0300

    Detect correct arguments

--- CSE344 - Systems Programming/HW01/replacer.c
+++ CSE344 - Systems Programming/HW01/replacer.c
@@ -1,14 +1,112 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include "replacer.h"
 
 int detect_arguments(int argc, char *argv[])
 {
-  // print all arguments
+  // print arguments
+  printf("argc: %d\n", argc);
   for (int i = 0; i < argc; i++)
   {
-    printf("argv[%d] = %s\n", i, argv[i]);
+    printf("argv[%d]: %s\n", i, argv[i]);
   }
 
-  printf("hello");
+  if (argc != 3)
+  {
+    printf("Invalid count");
+    usage_invalid();
+    return -1;
+  }
+  char *replace_pattern = argv[1];
+  char *file_name = argv[2];
+
+  // create ReplacePattern array
+  int pattern_count = 0;
+  ReplacePattern *replace_patterns = initialize_replace_patterns(replace_pattern, &pattern_count);
+
+  printf("hit\n");
+  printf("pattern count: %d\n", pattern_count);
+
+  // print ReplacePattern array
+  for (int i = 0; i < pattern_count; i++)
+  {
+    printf("replace: %s\n", replace_patterns[i].replace);
+    printf("with: %s\n", replace_patterns[i].with);
+    printf("case_sensitive: %d\n", replace_patterns[i].case_sensitive);
+  }
+
   return 0;
+}
+
+void usage_manual()
+{
+  printf("Usage: ./hw1 \"[replace pattern]\" inputFilePath\n"
+         "Replace Pattern Examples\n"
+         "Example: \"/str1/str2/\" \t\t -> Replace str2 with str1\n"
+         "Example: \"/str1/str2/i\" \t -> Casesensitive\n"
+         "Example: \"/str1/str2/;/str3/str4/\" -> Combine mutliple replace patterns  \n"
+         "Example: \"/[zs]tr1/str2/\" \t -> Multiple character match\n"
+         "Example: \"/^str1/str2/\" \t\t -> Match at the beginning of the line\n"
+         "Example: \"/str1$/str2/\" \t\t -> Match at the end of the line\n"
+         "Example: \"/st*r1/str2/\" \t\t -> Match any number of characters\n"
+         "Also you can combine multiple search patterns\n");
+}
+
+ReplacePattern *initialize_replace_patterns(const char *pattern, int *pattern_count)
+{
+  // detect ";" count in pattern
+  int count = 1;
+  for (size_t i = 0; i < strlen(pattern); i++)
+  {
+    if (pattern[i] == ';')
+    {
+      count++;
+    }
+  }
+
+  ReplacePattern *replace_patterns = (ReplacePattern *)malloc(sizeof(ReplacePattern) * count);
+  if (replace_patterns == NULL)
+  {
+    perror("malloc failed\n");
+    return NULL;
+  }
+  // initialize replace_patterns
+  for (int i = 0; i < count; i++)
+  {
+    replace_patterns[i].replace = NULL;
+    replace_patterns[i].with = NULL;
+    replace_patterns[i].case_sensitive = 0;
+    replace_patterns[i].match_beginning = 0;
+    replace_patterns[i].match_end = 0;
+    replace_patterns[i].match_any = 0;
+    replace_patterns[i].match_multiple = 0;
+  }
+  *pattern_count = count;
+  return replace_patterns;
+}
+
+void detect_replace_pattern(ReplacePattern* replace_patterns, const char* pattern)
+{
+  int count = 0;
+  const size_t size = strlen(pattern);
+  int last_index = 0;
+  for (int i = 0; i < count; i++)
+  {
+    int slash_count = 0;
+    for (size_t j = last_index; j < size; j++)
+    {
+      if (pattern[j] == '/')
+      {
+        slash_count++;
+      }
+    }
+  }
+}
+
+void usage_invalid()
+{
+  printf("Usage is invalid. See the manual\n");
+  usage_manual();
 }
--- CSE344 - Systems Programming/HW01/replacer.h
+++ CSE344 - Systems Programming/HW01/replacer.h
@@ -1,7 +1,37 @@
 #ifndef REPLACER_H
 #define REPLACER_H
 
+typedef struct
+{
+  char *replace;
+  char *with;
+  int case_sensitive;
+  int match_beginning;
+  int match_end;
+  int match_any;
+  int match_multiple;
+} ReplacePattern;
+
 int detect_arguments(int argc, char *argv[]);
 int open_file(char *file_name);
+
+// helper functions
+/**
+ * @brief print usage manual
+ */
+void usage_manual();
+
+void usage_invalid();
+
+/**
+ * @brief Create ReplacePattern array
+ *
+ * @param pattern The pattern that will be used to create ReplacePattern array
+ * @param pattern_count Detected pattern count
+ * @return ReplacePattern* The created ReplacePattern array
+ */
+ReplacePattern *initialize_replace_patterns(const char *pattern, int *pattern_count);
+
+void detect_replace_pattern(ReplacePattern *replace_pattern, const char *pattern);
 
 #endif // REPLACER_H
