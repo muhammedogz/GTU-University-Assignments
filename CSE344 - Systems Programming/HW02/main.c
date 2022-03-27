9c1f702574f4e9760ce0eecaadcb26b1b199b6a6
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2022-03-28 00:49:40 +0300

    Test main func

--- CSE344 - Systems Programming/HW02/main.c
+++ CSE344 - Systems Programming/HW02/main.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
+#include "frobenius.h"
+
+#define FILE_NAME "test.txt"
 
 int main()
 {
+  int file_descriptor = 0;
+  int file_size = 0;
+  char *file_content = NULL;
 
-  pid_t pid = fork();
-  int a = 5;
+  file_descriptor = open_file(FILE_NAME);
 
-  char *argv[] = {"./hw02", "hello", "world", "5", NULL};
-  char *env[] = {"PATH=/usr/bin", "/home/user", NULL};
+  file_content = read_file(file_descriptor, &file_size);
 
-  if (pid == 0)
-  {
-    execve("./exec", argv, env);
-  }
-  else 
-  {
-    wait(NULL);
-    printf("a = %d\n", a);
-    printf("I am parent\n");
-    printf("Parent id: %d\n", getpid());
-    printf("I am parent. My parent id %d\n", getppid());
-  }
+  printf("file content: %s\n", file_content);
 }
