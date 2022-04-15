c79046af5f9bb2cc1f8fcbb65948eb2c40046c4f
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2022-04-16 02:52:12 +0300

    Clear include libs

--- CSE344 - Systems Programming/Midterm Project/main_server.c
+++ CSE344 - Systems Programming/Midterm Project/main_server.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include <fcntl.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <errno.h>
 #include "include/common.h"
 #include "include/serverY.h"
 
